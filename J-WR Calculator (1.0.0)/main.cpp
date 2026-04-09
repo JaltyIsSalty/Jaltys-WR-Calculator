9132a507149d43bfae455c561b2ada42d3d57546
JaltyIsSalty <curlyhairedmesspersonal@gmail.com>
2026-04-09 16:05:15 -0500

    Add files via upload

---
+++ J-WR Calculator (1.0.0)/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+
+#include "src/GUI/mainGUI.h"
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	LaunchModMenu();
+	return 0;
+}
---
+++ J-WR Calculator (1.0.0)/src/WR/Info.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+namespace Info
+{
+	namespace defaults
+	{
+		constexpr double greyResistance = 60;
+		constexpr double reflector      = 0;
+		constexpr double defensePoints  = 0;
+	}
+}
